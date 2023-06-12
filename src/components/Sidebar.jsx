4327d1726034791686cc5407892ed77155c22098
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-13 01:06:31 +0530

    navlink active class style added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,30 +1,38 @@
 import React from "react";
 
 //packages
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 //icons
 import create from "../assets/icons/create.png";
 import film from "../assets/icons/film.png";
 import rightArrow from "../assets/icons/right-arrow.png";
 import logo from "../assets/icons/logo.png";
+import { useState } from "react";
 
 export const Sidebar = () => {
+    const [isActive, setIsActive] = useState("HOME");
     return (
-        <div className="bg-dark-blue h-full fixed w-[200px] overflow-hidden p-[25px]  flex flex-col gap-[40px]">
+        <div className="bg-dark-blue h-full fixed w-[200px] overflow-hidden p-[25px] px-[0] flex flex-col gap-[30px]">
             <Link
                 to="/"
-                className="cursor-pointe w-[45px] cursor-pointer"
+                className="cursor-pointe w-[45px] cursor-pointer ml-[25px]"
             >
                 <img
                     src={logo}
                     alt="logo"
                 />
             </Link>
-            <div className="flex flex-col gap-[30px] text-light-green">
-                <Link
+            <div className="flex flex-col text-light-green">
+                <NavLink
+                    style={({ isActive, isPending }) => {
+                        return {
+                            backgroundColor: isActive ? "red" : "",
+                            color: isPending ? "red" : "black",
+                        };
+                    }}
                     to={"/"}
-                    className="flex items-center gap-[6px]"
+                    className={`flex items-center px-[25px] py-[12px] gap-[6px] bg-[ #72c5f8]`}
                 >
                     <div className="w-[25px] h-[25px]">
                         <img
@@ -39,10 +47,16 @@ export const Sidebar = () => {
                             alt="arrow"
                         />
                     </div>
-                </Link>
-                <Link
+                </NavLink>
+                <NavLink
+                    style={({ isActive, isPending }) => {
+                        return {
+                            backgroundColor: isActive ? "red" : "",
+                            color: isPending ? "red" : "black",
+                        };
+                    }}
                     to={"/genres"}
-                    className="flex items-center gap-[6px]"
+                    className="flex items-center gap-[6px] px-[25px] py-[12px] "
                 >
                     <div className="w-[25px] h-[25px]">
                         <img
@@ -57,10 +71,16 @@ export const Sidebar = () => {
                             alt="arrow"
                         />
                     </div>
-                </Link>
-                <Link
+                </NavLink>
+                <NavLink
+                    style={({ isActive, isPending }) => {
+                        return {
+                            backgroundColor: isActive ? "red" : "",
+                            color: isPending ? "red" : "black",
+                        };
+                    }}
                     to={"/add-movie"}
-                    className="flex items-center gap-[6px]"
+                    className="flex items-center gap-[6px] px-[25px] py-[12px] "
                 >
                     <div className="w-[25px] h-[25px]">
                         <img
@@ -75,7 +95,7 @@ export const Sidebar = () => {
                             alt="arrow"
                         />
                     </div>
-                </Link>
+                </NavLink>
             </div>
         </div>
     );
