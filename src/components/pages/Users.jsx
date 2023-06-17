986b93c06284f5140a962b079543e87477ef83d4
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-17 18:20:01 +0530

    profiles section compleetd

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -30,7 +30,30 @@ const Users = () => {
     useEffect(() => {
         fetchUsers();
     }, []);
-    return <div>Users</div>;
+
+    return (
+        <section className="flex flex-col gap-[5px] relative pt-[50px]">
+            <div className="p-[10px] flex items-center justify-between absolute top-0 left-0 w-full bg-blue-text ">
+                <p className="text-[15px]">Profile</p>
+                <p className="text-[15px]">Name</p>
+                <p className="text-[15px]">Email</p>
+                <p className="text-[15px]">Admin</p>
+            </div>
+            {users?.map((user) => (
+                <div className="p-[10px] flex items-center justify-between bg-[#275161]">
+                    <div className="w-[50px] h-[50px] rounded-full overflow-hidden">
+                        <img
+                            src={user?.profilePic.url}
+                            alt="profile"
+                        />
+                    </div>
+                    <p className="text-[15px]">{user?.username}</p>
+                    <p className="text-[15px]">{user?.email || "-----------------------"}</p>
+                    <p className="text-[15px]">User</p>
+                </div>
+            ))}
+        </section>
+    );
 };
 
 export default Users;
