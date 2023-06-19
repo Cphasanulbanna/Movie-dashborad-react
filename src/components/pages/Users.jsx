f04253c4210d9e7987c45c444049453305c457b9
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-19 22:28:33 +0530

    updated users page

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -11,12 +11,9 @@ import axiosConfig from "../../../axiosConfig";
 const Users = () => {
     const [users, setUsers] = useState([]);
 
-    console.log(users, "users-------------");
-
     const { userdata } = useUserDataStore();
     const access_token = userdata?.access_token;
     const fetchUsers = async () => {
-        console.log("eneterd");
         try {
             const controller = new AbortController();
             const response = await axiosConfig.get("/auth/users", {
@@ -28,9 +25,7 @@ const Users = () => {
 
             setUsers(response.data?.users);
             controller.abort();
-        } catch (error) {
-            console.log(error);
-        }
+        } catch (error) {}
     };
     useEffect(() => {
         fetchUsers();
