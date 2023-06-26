362b4f89cd0ea10e5bfe4226823a043c40564c02
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-26 17:14:31 +0530

    delete modal responsive completed

--- src/components/modals/ConfirmDelete.jsx
+++ src/components/modals/ConfirmDelete.jsx
@@ -5,43 +5,41 @@ import remove from "../../assets/icons/confirm-delete.png";
 import close from "../../assets/icons/close.png";
 
 //components
-import ModalWrapper from "../general/ModalWrapper";
 import ButtonLoader from "../general/Button-loader/ButtonLoader";
 
 const ConfirmDelete = ({ deleteItem, closeModal, state, buttonLoader }) => {
     return (
-        <ModalWrapper
-            state={state}
-            closeModal={closeModal}
-            style={{ width: "30%", background: "#fff" }}
+        <section
+            className={`overlay flex justify-center items-center ${state ? "visible" : "hidden"}`}
+            onClick={closeModal}
         >
-            <div className="flex flex-col justify-center items-center gap-[20px] relative">
-                <div className="w-[60px] h-[60px]">
+            <div className="flex flex-col justify-center items-center bg-text-white p-[30px] rounded-[10px] gap-[20px] relative lg2:w-[375px] sm3:w-[300px]">
+                <div className="w-[60px] h-[60px] lg2:w-[45px] lg2:h-[45px]">
                     <img
                         src={remove}
                         alt="delete-icon"
                     />
                 </div>
-                <h1 className="text-[22px] font-bold w-[75%] mx-auto text-center text-[#111]">
+                <h1 className="text-[22px] font-bold w-[75%] mx-auto text-center text-[#111] lg2:text-[18px]">
                     Are you sure you want to delete this?
                 </h1>
                 <div className="flex gap-[15px] justify-center items-center">
                     <div
                         onClick={closeModal}
-                        className="btn bg-[#bbbbbb] text-light-white"
+                        className="btn bg-[#bbbbbb] text-light-white lg2:h-[40px]"
                     >
                         Cancel
                     </div>
                     <div
                         onClick={deleteItem}
-                        className="btn bg-[#f7215a] min-w-[100px] w-[100%] text-light-white h-[44px]"
+                        className="btn bg-[#f7215a] min-w-[100px] w-[100%] text-light-white h-[44px] lg2:h-[40px]"
                     >
                         {buttonLoader ? <ButtonLoader size={0.3} /> : "Delete"}
                     </div>
                 </div>
                 <div
                     onClick={closeModal}
-                    className="w-[30px] h-[30px] bg-light-grey-2 cursor-pointer rounded-[50%] absolute overflow-hidden p-[7px] right-[-40px] top-[-40px] hover:opacity-[0.8]  border-[2px] border-solid border-light-grey
+                    className="w-[30px] h-[30px] bg-light-grey-2 cursor-pointer rounded-[50%] absolute overflow-hidden p-[7px] right-[-10px] top-[-10px]  lg2:w-[22px] lg2:h-[22px] lg2:p-[4px] hover:opacity-[0.8]  border-[2px] border-solid border-light-grey
                     "
                 >
                     <img
@@ -50,7 +48,7 @@ const ConfirmDelete = ({ deleteItem, closeModal, state, buttonLoader }) => {
                     />
                 </div>
             </div>
-        </ModalWrapper>
+        </section>
     );
 };
 
